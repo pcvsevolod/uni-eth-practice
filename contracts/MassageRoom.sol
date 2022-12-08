9bd47e0133f4fb07321d7697122b8d48943696db
V. Shapovalov <pcvsevolod@gmail.com>
2022-12-08 16:36:06 +0300

    add: worker registration and approval

--- contracts/MassageRoom.sol
+++ contracts/MassageRoom.sol
@@ -1,7 +1,76 @@
 pragma solidity ^0.8.13;
 
+contract WorkerManager {
+    struct Request {
+        string name;
+        bool isHere;
+    }
+
+    struct Worker {
+        string name;
+        bool isHere;
+    }
+
+    mapping(address => Request) requests;
+    mapping(address => Worker) workers;
+
+    function putRequestToRegister(address addr, string memory name) external {
+        requests[addr] = Request(name, true);
+    }
+
+    function approveRequest(address addr) external {
+        requests[addr].isHere = false;
+        workers[addr] = Worker(requests[addr].name, true);
+    }
+
+    function isRequestHere(address addr) external view returns (bool) {
+        return requests[addr].isHere;
+    }
+
+    function isWorkerHere(address addr) external view returns (bool) {
+        return workers[addr].isHere;
+    }
+}
+
 contract MassageRoom {
+    address deployer;
+
+    WorkerManager workerManager;
+
+    constructor() {
+        deployer = msg.sender;
+
+        workerManager = new WorkerManager();
+    }
+
+    modifier _canApproveWorkerRegistrationRequests() {
+        require(
+            msg.sender == deployer,
+            "Only deployer can approve worker registration requests"
+        );
+        _;
+    }
+
     function getTestValue() external pure returns (uint256) {
         return 3;
+    }
+
+    function askToRegisterAsWorker(string memory name) external {
+        workerManager.putRequestToRegister(msg.sender, name);
+    }
+
+    function approveWorkerRegistrationRequest(address workerAddr)
+        external
+        _canApproveWorkerRegistrationRequests
+    {
+        workerManager.approveRequest(workerAddr);
+    }
+
+    function haveIAskedToBeAWorker() external view returns (bool) {
+        return workerManager.isRequestHere(msg.sender);
+    }
+
+    function amIAWorker() external view returns (bool) {
+        return workerManager.isWorkerHere(msg.sender);
     }
 }
--- test/TestMassageRoom.js
+++ test/TestMassageRoom.js
@@ -3,12 +3,37 @@ const MassageRoom = artifacts.require('MassageRoom');
 contract('MassageRoom', (accounts) => {
   let contract;
 
+  const admin = {
+    addr: accounts[0],
+  };
+
+  const worker = {
+    addr: accounts[1],
+    name: 'Worker Zero',
+  };
+
   beforeEach('Clearing contract', async () => {
-    contract = await MassageRoom.new({ from: accounts[0] });
+    contract = await MassageRoom.new({ from: admin.addr });
   });
 
   it('Should get test value', async () => {
     const testValue = await contract.getTestValue.call({ from: accounts[1] });
     assert.equal(testValue, 3, 'Wrong test value');
+  });
+
+  describe('Working with workers', () => {
+    it('Should register as worker', async () => {
+      await contract.askToRegisterAsWorker(worker.name, { from: worker.addr });
+      const requestPut = await contract.haveIAskedToBeAWorker.call({ from: worker.addr });
+      const amIAWorker = await contract.amIAWorker.call({ from: worker.addr });
+      assert.equal(requestPut, true, 'Request was not put');
+      assert.equal(amIAWorker, false, 'Worker without admin permission');
+
+      await contract.approveWorkerRegistrationRequest(worker.addr, { from: admin.addr });
+      const requestPutAfter = await contract.haveIAskedToBeAWorker.call({ from: worker.addr });
+      const amIAWorkerAfter = await contract.amIAWorker.call({ from: worker.addr });
+      assert.equal(amIAWorkerAfter, true, 'Request was not approved');
+      assert.equal(requestPutAfter, false, 'Request is still here');
+    });
   });
 });
